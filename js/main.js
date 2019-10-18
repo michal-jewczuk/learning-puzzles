4c1a690be12928b9ccb3e3de313ea3f19df998ef
Michal Jewczuk <michal-jewczuk@outlook.com>
2019-10-18 13:27:59 +0200

    splitted into 3 modules; added option to check if in correct place; added option to generate puzzles and background at start

--- js/main.js
+++ js/main.js
@@ -1,15 +1,103 @@
+
+const targetArea = document.getElementById('solution-area');
+const puzzleArea = document.getElementById('puzzles-area');
+const solutionClass = 'solution-background';
+const puzzleClass = 'playable';
+
 MainModule = {
 
-	DOMel: {
-		playableArea: document.getElementById('playable-section'),
-		targetArea: document.getElementById('target-area'),
-		puzzleArea: document.getElementById('puzzles-area'),
-		puzzles: Array.from(document.getElementsByClassName('playable')),
-		backgrounds: Array.from(document.getElementsByClassName('main-area-background'))
+	init: () => {
+		GameModule.setUpGame();
+		UIModule.registerEvents();
 	},
 
-	init: () => {
-		MainModule.registerEvents();
+	checkIfCorrect: (target, child) => {
+		const targetId = MainModule.extractId(target.id);
+		const childId = MainModule.extractId(child.id);
+		if (targetId === childId) {
+			MainModule.applyCorrect(child);
+		} else {
+			MainModule.applyIncorrect(child);
+		}
+	},
+
+	applyCorrect: (element) => {
+		element.classList.remove('incorrect');
+		element.classList.add('correct');
+	},
+
+	applyIncorrect: (element) => {
+		element.classList.remove('correct');
+		element.classList.add('incorrect');
+	},
+
+	applyNone: (element) => {
+		element.classList.remove('correct');
+		element.classList.remove('incorrect');
+	},
+
+	extractId: (name) => {
+		return parseInt(name.slice(1));
+	},
+
+
+};
+
+GameModule = {
+
+	setUpGame: () => {
+		const count = 9;
+		GameModule.createTargetGrid(count);
+		GameModule.createPuzzles(count);
+	},
+
+	createTargetGrid: (count) => {
+		let html = '';
+		for (let i = 1; i <= count; i++) {
+			html += '<div class="' + solutionClass + '" id="b' + i + '"></div>';
+
+		}
+		targetArea.innerHTML = html;
+	},
+
+	createPuzzles: (count) => {
+		const puzzles = new Array();
+		let html = '';
+		for (let i = 1; i <= count; i++) {
+			puzzles.push(GameModule.createPuzzle(i));
+
+		}
+		puzzles.sort(() => Math.random() - 0.5);
+		puzzleArea.innerHTML = puzzles.reduce((e1, e2) => e1 + e2);
+	},
+
+	createPuzzle: (id) => {
+		let html = '<div class="' + puzzleClass + '" id="p' + id + '" draggable="true">';
+		html += '<p>element #' + id + '</p>';
+		html += '</div>';
+		return html;
+	}
+
+	
+};
+
+UIModule = {
+
+	registerEvents: () => {
+	        puzzleArea.addEventListener('drop', UIModule.drop);	
+		puzzleArea.addEventListener('dragover', UIModule.allowDrop);
+
+		const backgrounds = Array.from(document.getElementsByClassName(solutionClass));
+		const puzzles = Array.from(document.getElementsByClassName(puzzleClass));
+
+		puzzles.forEach( (puzzle) => {
+			puzzle.addEventListener('dragstart', UIModule.drag);
+		});
+
+		backgrounds.forEach( (bg) => {
+	        	bg.addEventListener('drop', UIModule.drop);	
+			bg.addEventListener('dragover', UIModule.allowDrop);
+		});
 	},
 
 	allowDrop: (ev) => {
@@ -24,27 +112,25 @@ MainModule = {
 		ev.preventDefault();
 		const data = ev.dataTransfer.getData("childId");
 		const child = document.getElementById(data);
-		if (ev.target.className === 'main-area-background'
-			|| ev.target.id === 'puzzles-area') {
-			ev.target.appendChild(child);
+
+		if (child === null) {
+			return true;
 		}
-	},
 
-	registerEvents: () => {
-	        MainModule.DOMel.puzzleArea.addEventListener('drop', MainModule.drop);	
-		MainModule.DOMel.puzzleArea.addEventListener('dragover', MainModule.allowDrop);
-
-		MainModule.DOMel.puzzles.forEach( (puzzle) => {
-			puzzle.addEventListener('dragstart', MainModule.drag);
-		});
-
-		MainModule.DOMel.backgrounds.forEach( (bg) => {
-	        	bg.addEventListener('drop', MainModule.drop);	
-			bg.addEventListener('dragover', MainModule.allowDrop);
-		});
+		if (ev.target.className === solutionClass) {
+			ev.target.appendChild(child);
+			MainModule.checkIfCorrect(ev.target, child);
+		} else if (ev.target.id === 'puzzles-area') {
+			ev.target.appendChild(child);
+			MainModule.applyNone(child);
+		}
 	}
 
 };
+
+
+
+
 
 MainModule.init();
 
